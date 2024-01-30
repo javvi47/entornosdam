745630b5d4f58ab1e9c35a425e31c10bd620294a
asir21-05 <asir21-05@c16x14.iescierva.net>
2024-01-30 09:27:44 +0100

    ED1 Mostrar mi nombre en pantalla

--- ejemplo1.java
+++ ejemplo1.java
@@ -1,5 +1,7 @@
 public class Ejemplo1 {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO");
+        String nombre = "Javi";
+        System.out.println("Mi nombre es " + nombre)
     }
 }
